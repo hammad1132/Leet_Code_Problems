8abe3b7e560367e9523975cf9e84c70d3e962da8
HAMMAD ANSARI <hammadansari1905@gmail.com>
2026-07-11 16:18:55 +0530

    Time: 0 ms (100%), Space: 7.8 MB (38.12%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -2,8 +2,15 @@ class Solution {
 public:
     int fib(int n) {
         if(n==0)return 0;
-        if(n==1)return 1;
-
-        return fib(n-2)+fib(n-1);
+         int a=0;
+     int b=1;
+     int c=1;
+     for(int i=1;i<n;i++)
+     { 
+         c=a+b;
+         a=b;
+         b=c;
+       
+    }return c;
     }
 };
